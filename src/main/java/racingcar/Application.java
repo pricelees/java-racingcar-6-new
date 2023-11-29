325d85d0b8eacb4bfb42bcf59ccf53e1fd9a5216
sangdol <priceelees@gmail.com>
2023-11-29 11:13:33 +0900

    feat(Application): 게임을 실행하는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.config.Configuration;
+import racingcar.controller.CarRacingController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarRacingController carRacingController = Configuration.carRacingController();
+        carRacingController.run();
     }
 }
